98cfad7a5e313fc119bfc56e52c4c7e585c6303b
DongyeongKim <dongyeongkim33@gmail.com>
2023-02-26 03:33:55 +0900

    first week problem

---
+++ lab/week1/problem1.py
@@ -0,0 +1,24 @@
+from jax import grad
+from random import random 
+from typing import Callable
+
+
+##                        Problem 1                           ##
+##                                                            ##
+##         Arbitrary quadratic function will be given.        ##
+## Return the optimal point(global minimum) of given function ##
+##                                                            ##
+##                  Made by @jangyoujin0917                   ##
+##                                                            ##
+
+
+def test_func(x): # function for testing; evaluation will be different.
+    return x ** 2
+
+
+def solution(func: Callable, start_point: float): # DO NOT MODIFY FUNCTION NAME
+    ### IMPLEMENT FROM HERE
+    
+
+if __name__ == '__main__':
+    print(solution(test_func, 10*random()))
